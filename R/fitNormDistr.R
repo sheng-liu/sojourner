e58f92ed4c18963774f225bb094e3f00f39fe190
Youngsoo Sung <ysung6@jhu.edu>
2018-07-17 18:38:32 -0400

    fitNormDistr.R - combined with fitNormDistr.manual.R so that it can run with additional parameters(mean,sd,proportion). Does not generate outputfile for seed value anymore. fitNormDistr.R - removed from package. getCI.R - handles single and multicomponent distributions and boot strapping is done. Single and multi component cases are handled differently.

--- R/fitNormDistr.R
+++ R/fitNormDistr.R
@@ -10,18 +10,22 @@
 ##' @description fit normal distributions to diffusion coefficient caclulated by Dcoef method.
 ##'
 ##' @usage
-##' fitNormDistr(dcoef,components=NULL,log.transform=F,binwidth=NULL,
-##'              combine.plot=F,output=F,seed=NULL)
+##' fitNormDistr(dcoef,components=NULL,log.transform=F,binwidth=NULL,combine.plot=F,output=F,
+##'              seed=NULL,meanRange=NULL, proportion=NULL, means=NULL, sd=NULL)
 ##' @param dcoef diffusion coefficient calculated from Dcoef().
 ##' @param components parameter specifying the number of components to fit. If NULL (default), a components analysis is done to determine the most likely components and this number is then used for subsequent analysis.
 ##' @param log.transform logical indicate if log10 transformation is needed, default F.
 ##' @param binwidth binwidth for the combined plot. If NULL (default), will automatic assign binwidth.
-##' @param output Logical indicaring if output file should be generated.
-##' @param combine.plot Logical indicating if all the plot should be combined into one, with same scale (/same axises breaks), same color theme, and same bin size for comparison.
+##' @param combine.plot Logical indicate if all the plot should be combined into one, with same scale (/same axises breaks), same color theme, and same bin size for comparison.
+##' @param output Logical indicate if output file should be generated.
 ##' @param seed Seed for random number generator. This makes each run easily repeatable. Seed will be automatically assigned if no seed is specified (default). The seed information is stored as an attribute of the returned object. The seed can also be output to a txt file when output=T.
+##' @param meanRange list of vectors, each of which have the min&max that estimates a range where a peak of the distribution should lie.
+##' @param proportion numeric vector with estimates of each component's proportion of the whole data.
+##' @param means numeric vector with estimates of mean(mu) values for each component.
+##' @param sd numeric vector with estimates of standard deviation(sigma) values for each component.
 ##' @details
 ##' components analysis uses the likelihood ratio test (LRT) to assess the number of mixture components.
-##' Bad Random seed generation may cause normalmixEM to crash. running the function again would be the quickest solution to this issue.
+##' Bad Random seed generation may cause normalmixEM to crash. Running the function again would be the quickest solution to this issue.
 ##'
 ##' @return
 ##' \describe{
@@ -32,7 +36,6 @@
 ##' }
 
 ##' @examples
-##'
 ##' # compare folders
 ##' folder1=system.file("extdata","SWR1",package="sojourner")
 ##' folder2=system.file("extdata","HTZ1",package="sojourner")
@@ -47,13 +50,13 @@
 ##' mapply(identical,a[[1]],b[[1]])
 ##' #try with log transformation
 ##' c=fitNormDistr(dcoef,components=2,log.transform=T,combine.plot=F,output=F)
+##' #trying with some parameters provided(this will be applied to all dcoef results)
+##' d=fitNormDistr(dcoef,meanRange=list(c(0.1,0.3), c(0.5,0.7)))
 
 
 ##' @export fitNormDistr
 ###############################################################################
 # fit normal distribution to diffusion coefficient
-
-# library(mixtools)
 
 #function for seed setting
 .setSeed=function(seed=NULL){
@@ -65,7 +68,7 @@
     
     note=paste("\nRandom number generation seed",seed,"\n")
     cat(note)
-    return(note)
+    return(seed)
 }
 
 #function that deals with one-component normal distribution fitting
@@ -77,7 +80,7 @@
     #a list with components similar to that of mixEM so that it can be used in gg.mixEM
     fit.output = list(x=as.vector(data), lambda = c(1), mu = fit.info$estimate[1], sigma = fit.info$sd[1],
                       loglik = fit.info$loglik, restarts=0, ft="normalmixEM", posterior= dummy.posterior)
-    fit.se = list(mu.se= c(fit.info$estimate[2]), sigma.se = c(fit.info$sd[2]), lambda.se = c(0))
+    fit.se = list(mu.se= c(fit.info$estimate[2]), sigma.se = c(fit.info$sd[2]), lambda.se = c(0.00))
     return(list(fit.output,fit.se))
 }
 
@@ -96,8 +99,10 @@
         data.frame(proportion=result$lambda,
                    proportion.se=result.se$lambda.se),
         
-        data.frame(mean=if(log.transform) 10^(result$mu)
-                   else result$mu),
+        #why returning it back from log?
+        #data.frame(mean=if(log.transform) 10^(result$mu)
+        #           else result$mu),
+        data.frame(mean=result$mu),
         mean.se=as.vector(result.se$mu.se),
         
         
@@ -107,21 +112,20 @@
         log.lik=result$loglik
     )
     
-    print(class(out))
-    print(list(out))
-    print(lapply(out, length))
-    print(out)
     out=list(out)
     out=t(do.call(cbind.data.frame,out))
     colnames(out) = NULL
     return(out)
 }
 
-.fitNormDistr=function(dcoef,components=NULL,log.transform=F,binwidth=NULL,combine.plot=F,output=F){
-    auto=FALSE
+.fitNormDistr=function(dcoef,components=NULL,log.transform=F,binwidth=NULL,combine.plot=F,output=F,meanRange=NULL,proportion=NULL,means=NULL,sd=NULL, seed=NULL){
     # scale=1e3
+    if(is.null(seed)){stop("NULL seed, cannot execute .fitNormDistr")}
     name=names(dcoef)
     len=length(dcoef)
+    
+    #parameters that help initial settings.
+    params = list(meanRange, proportion, means, sd)
 
     mixmdl.lst=list()
     length(mixmdl.lst)=len
@@ -131,10 +135,16 @@
     mixmdl.se.lst=list()
     length(mixmdl.se.lst)=len
     names(mixmdl.se.lst)=name
-
+    
+    ## This part deals with input related errors
+    #collection of parameter vector lengths
+    param.len = lapply(params, length)
+    param.len=param.len[param.len!=0]
+    if(!is.null(components) & (any(param.len != components))){
+        stop("Invalid parameter size: number of components and parameter vector length does not match")
+    }
 
     for (i in 1:len){
-
         data=dcoef[[name[i]]][,"slope"]
 
         # log transformation
@@ -142,46 +152,55 @@
             data=log10(data)
             data=data[!is.na(data)]
         }
-
-        # components test
-        if (is.null(components)){
+        
+        components.int=0
+        #components test
+        if (is.null(components)&length(param.len)==0){
             components.int = .getCompNum(data)
-            auto=T
         }else{
-            components.int=components
+            if(length(param.len)==0){
+                components.int=components
+            }else if(!all(as.vector(param.len)==param.len[[1]])){
+                stop("Parameter vector lengths do not match")
+            }
+            if(is.null(components)){
+                components.int=param.len[[1]]
+            } else{components.int=components}
         }
-
-
-        if (components.int==1){#The single component is a special case here since it cannot be a mixEM class object
+        
+        if(!is.null(meanRange)){
+            if (length(param.len) == 1){
+                #try to estimate/extract mu and sigma values from the given ranges
+                means=unlist(lapply(meanRange, mean))
+                diffs=lapply(meanRange, function(x){max(x)-mean(x)})
+                sd=unlist(diffs)/1.6
+            }
+            else{stop("Either only meanRange or any of proportion/mean/sd should be provided")}
+        }
+        
+        if (components.int==1){ #The single component is a special case here since it cannot be a mixEM class object
             oneComp=.singlecompFit(data=data)
             fit.output=oneComp[[1]]
             fit.se=oneComp[[2]]
             plot.mixEM=gg.mixEM(fit.output,binwidth=binwidth,reorder=F)
-            
+            mixmdl=fit.output
             mixmdl.lst[[i]] = fit.output
+            mixmdl.se=fit.se
             mixmdl.se.lst[[i]] = fit.se
         } else{
-            # convergence creteria epsilon = 1e-10 is to filter out false positives
-            mixmdl=normalmixEM(data,k=components.int,maxit=1e4,epsilon = 1e-10, lambda = c(0.7,0.3))
-
+            mixmdl=normalmixEM(data,k=components.int,maxit=1e4,epsilon=1e-10, lambda=proportion, mu=means, sigma=sd)
             # reorder also for plotting and for output result
             # this is recordered on mean value
             mixmdl=reorderEM(mixmdl)
-
             print(summary(mixmdl))
-            # plot(mixmdl,which=2)
             plot.mixEM=gg.mixEM(mixmdl,binwidth=binwidth,reorder=T)
-
-            mixmdl.lst[[i]]=mixmdl
-
             # approximate standard error using parametic bootstrap
             cat("\napproximating standard error by parametic bootstrap...\n\n")
             capture.output({mixmdl.se=boot.se(mixmdl, B = 100)})
             # file="/dev/null" # this only for mac
-
+            mixmdl.lst[[i]]=mixmdl
             mixmdl.se.lst[[i]]=mixmdl.se
         }
-        # supprress warnings
         suppressMessages(plot(plot.mixEM))
     }
 
@@ -232,27 +251,19 @@
 
     # output
     if (output==T){
-        if(auto){
-            for (i in 1:length(result.lst)){
-                result.df=do.call(rbind.data.frame,list(result.lst[[i]]))
-                fileName=paste("FitNormDistr-",
-                               .timeStamp(name[i]),"....csv",sep="")
-                cat("\nOutput FitNormDistr for multiple files\n")
-                write.csv(file=fileName,result.df)
-            }
-        }else{
-            result.df=do.call(rbind.data.frame,result.lst)
-            fileName=paste("FitNormDistr-",
-                       .timeStamp(name[1]),"....csv",sep="")
-            cat("\nOutput FitNormDistr.\n")
-            write.csv(file=fileName,result.df)
-        }
+        result.df=do.call(rbind.data.frame,result.lst)
+        logTrans=""
+        if(log.transform){logTrans=".logtrans"}
+        fileName=paste("FitNormDistr-",
+                   .timeStamp(name[1]),".seed",seed,logTrans,"....csv",sep="")
+        cat("\nOutput FitNormDistr.\n")
+        write.csv(file=fileName,result.df)
         
         # output plot
         if (combine.plot==T){
 
             fileName=paste("FitNormDistr-combinePlot-",
-                           .timeStamp(name[1]),"....pdf",sep="")
+                           .timeStamp(name[1]),".seed",seed,logTrans,"....pdf",sep="")
             cat("\nOutput FitNormDistr plot.\n")
 
             # gridExtra::marrangeGrob non-interactive use, multipage pdf
@@ -266,22 +277,13 @@
 }
 
 
-fitNormDistr=function(dcoef,components=NULL,log.transform=F,binwidth=NULL,combine.plot=F,output=F,seed=NULL){
-    note=.setSeed(seed=seed)
+fitNormDistr=function(dcoef,components=NULL,log.transform=F,binwidth=NULL,combine.plot=F,output=F,seed=NULL, meanRange=NULL, proportion=NULL, means=NULL, sd=NULL){
+    seed=.setSeed(seed=seed)
 
-    # output seed
-    if (output==T){
-
-        name=names(dcoef)
-        fileName=paste("FitNormDistr-",
-                       .timeStamp(name[1]),"-Seed....txt",sep="")
-        writeLines(text=note,con=fileName)
-    }
-    
     # return
-    structure(.fitNormDistr(dcoef=dcoef,components=components,log.transform=log.transform,binwidth=binwidth,combine.plot=combine.plot,output=output),seed=seed)
+    structure(.fitNormDistr(dcoef=dcoef,components=components,log.transform=log.transform,binwidth=binwidth,combine.plot=combine.plot,output=output,
+                            meanRange=meanRange,proportion=proportion,means=means,sd=sd,seed=seed),seed=seed)
 }
-
 # TODO:
 # 1 check how the se is calculated for the 1 component case and for the multi-component cases
 # 2 combine the output of seed into the df
--- R/getCI.R
+++ R/getCI.R
@@ -7,31 +7,39 @@
 ##' @title getCI
 ##' @rdname getCI-methods
 ##' @docType methods
-##' @description given number of samples, return Confidence Interval information(mean, bounds, std err) for diffusion
+##' @description given a dcoef result, return Confidence Interval information(mean, bounds, std err) for diffusion
 ##' coefficient and the proportions of each components.
 ##'
 ##' @usage
-##' getCI(samples,confidence=0.95,output=F)
-##' @param samples diffusion coefficient calculated from Dcoef().
+##' getCI(dcoef.result,confidence=0.95,output=F,n.reps=100,meanRange=NULL,proportion=NULL,means=NULL,sd=NULL,log.transform=F,components=NULL)
+##' @param dcoef.result diffusion coefficient calculated from Dcoef().
 ##' @param confidence the level of confidence that is used to calculate the confidence interval.
-##' @param output Logical indicating if output file should be generated.
+##' @param output Logical indicate if output file should be generated.
+##' @param n.reps number of replicates for bootstrapping
+##' @param meanRange list of vectors, each of which have the min&max that estimates a range where a peak of the distribution should lie.
+##' @param proportion numeric vector with estimates of each component's proportion of the whole data. Values in the vector would add up to 1.
+##' @param means numeric vector with estimates of mean(mu) values for each component.
+##' @param sd numeric vector with estimates of standard deviation(sigma) values for each component.
+##' @param log.transform logical indicate if log10 transformation is needed, default F.
+##' @param components parameter specifying the number of components to fit. If NULL (default), a components analysis is done to determine the most likely components and this number is then used for subsequent analysis.
 ##' @details
-##' Supplied with number of samples as dcoef, uses that to calculate the confidence range.
-##' Each entry in dcoeflist will consist of number of samples regarding that data.
-##' May hit an error for bootstrapping due to random number generation, but re-running the function will do the job.
+##' Supplied with a dcoef output, uses that to bootstrap and calculate the confidence range.
+##' May hit an error for bootstrapping due to random number generation, but re-running the function will very likely resolve the problem.
+##' The t-distribution/critical-t was used to calculate the Confidence Interval. 
+##' For multi-component distributions, the boot.se function from the mixtools package was used.
+##' For single-component distributions, a separate function was used to calculate the stderr and confidencet interval.
 ##' 
 ##'
 ##' @return
-##' list of items, each of which will contain:
+##' list of items, each of which will contain for each distribution component:
 ##' \describe{
 ##' \item{Estimate}{Mean estimated from sample}
 ##' \item{CI lower}{Lower bound of the confidence interval}
 ##' \item{CI upper}{Upper bound of the confidence interval}
 ##' \item{Std. Error}{Std. Error for given data}
 ##' }
-
-##' @examples
 ##'
+##' @examples
 ##' # compare folders
 ##' folder1=system.file("extdata","SWR1",package="sojourner")
 ##' folder2=system.file("extdata","HTZ1",package="sojourner")
@@ -40,51 +48,80 @@
 ##' MSD=msd(trackll=trackll)
 ##' #run Dcoef()
 ##' dcoef=Dcoef(MSD,dt=6,plot=T,output=F)
-##' #resample data with bootstrapping(get 10 resampled data)
-##' bootstraps=bootstrap(dcoef, attribute="slope")
-##' # get confidence interval for this bootstrapped samples
-##' a=getCI(bootstraps)
-##' # to manually set confidence to 80% and generate output csv file
-##' b=getCI(bootstraps, confidence=0.8, output=T)
+##' # get confidence intervals for this dcoef result which contains data from two different folders
+##' a=getCI(dcoef)
+##' # to manually set confidence to 80%
+##' b=getCI(dcoef, confidence=0.8, output=F)
+##' # get confidence interval with the some estimate values specified, same specification will be used for both data
+##' e=getCI(dcoef, confidence=0.95, means=c(0.1, 0.4))
+##' # If you want to apply to just one of this dcoef results
+##' f=getCI(dcoef[1], confidence=0.95, means=c(0.1,0.4))
 ##' 
-##' @importFrom gmodels ci
-## we deal with list of samples(possibly resampled)
 ###TODO add plot with confidence interval
-require(gmodels)
 
-##helper fucntion for calculating confidence interval for given attribute
-.extractCI=function(fitResult, data, component, confidence){
-    wanted=lapply(fitResult, get, x=data)
-    return(ci(t(as.data.frame(wanted))[,component], confidence=confidence))
+##small helper function that calculates the CI in terms of stderr. This returns the critical t-values, given the confidence.
+.get.seRange=function(confidence, num.samples){
+    diff=(1-confidence)/2
+    range.vector=c(diff, 1-diff)
+    range.se=qt(range.vector, num.samples-1)
+    print(range.se)
+    return(range.se)
+}
+
+##bootstrap and then return stderr for means 
+##Ref: http://www.stat.wisc.edu/~larget/stat302/chap3.pdf
+.boot.se.onecomp=function(fitResult, n.reps=100){
+    data=fitResult$x
+    #for choosing in bootstraps
+    f=function(d,i){d[i]}
+    bootstraps = boot::boot(data,f,n.reps)
+    means=apply(bootstraps$t, 1, mean)
+    list(mu.se=sd(means), lambda.se=0)
+}
+
+.boot.se.all=function(fitResult, B){
+    if(length(fitResult$mu) == 1){
+        return(.boot.se.onecomp(fitResult,B))
+    } else {
+        capture.output({multi.stderr=boot.se(fitResult, B)})
+        return(multi.stderr)
+    }
 }
 
 ##' @export getCI
-getCI=function(samples, confidence=0.95, output=F){
-    inputNames = names(samples)
+getCI=function(dcoef.result, confidence=0.95, output=F, n.reps=100, meanRange=NULL, proportion=NULL, means=NULL, sd=NULL, log.transform=F, components=NULL){
+    #from the desired confidecne lvl, calculate the crtitcal-t value
+    SE.range=.get.seRange(confidence, n.reps)
+    inputNames = names(dcoef.result)
     ciList = list()
-    for (i in 1:length(samples)){
-        distances=list()
-        #reformat the samples prior to running fitNormDistr
-        boot.list=as.list(data.frame(t(samples[[i]])))
-        boot.matricies = lapply(boot.list, as.matrix)
-        listlen=length(boot.matricies)
-        for (j in 1:listlen) {
-            #fitNormDistr has to recognize this column name.
-            colnames(boot.matricies[[j]]) <- "slope"
+    #fit and get multiple components
+    fit.result=fitNormDistr(dcoef.result, meanRange=meanRange, proportion=proportion, means=means, sd=sd, log.transform=log.transform, components=components)
+    #generate std.err value
+    fit.se=lapply(fit.result, .boot.se.all, B=n.reps)
+    for (i in 1:length(fit.result)){
+        out.df = data.frame()
+        
+        #Extract values form the fitting result and then apply the calculated critical-t values stored in SE.range
+        #generate the rownames in meannames and proportionnames each time we do this
+        meannames=c()
+        for (j in 1:length(fit.result[[i]]$mu)) {
+            out.df = rbind(out.df, c(Estimate=fit.result[[i]]$mu[j], CI.lower=SE.range[1]*fit.se[[i]]$mu.se[j]+fit.result[[i]]$mu[j], 
+                                     CI.upper=SE.range[2]*fit.se[[i]]$mu.se[j]+fit.result[[i]]$mu[j], Std.Error=fit.se[[i]]$mu.se[j]))
+            meannames=c(meannames, paste(j,"-compCI",sep=""))
         }
-        #fit.result = fitNormDistr(boot.matricies, components = 2)
-        fit.result=lapply(boot.matricies, fitNormDistr.manual, means=c(0.0,1.0))
+        proportionnames=c()
+        for (j in 1:length(fit.result[[i]]$lambda)) {
+            out.df = rbind(out.df, c(Estimate=fit.result[[i]]$lambda[j], CI.lower=SE.range[1]*fit.se[[i]]$lambda.se[j]+fit.result[[i]]$lambda[j], 
+                                     CI.upper=SE.range[2]*fit.se[[i]]$lambda.se[j]+fit.result[[i]]$lambda[j], Std.Error=fit.se[[i]]$lambda.se[j]))
+            proportionnames=c(proportionnames, paste(j,"-ProportionCI", sep=""))
+        }
         
-        #calculate confidence intervals for each components.
-        firstcompCI=.extractCI(fit.result, "mu", 1, confidence)
-        secondcompCI=.extractCI(fit.result, "mu", 2, confidence)
-        firstProportionCI = .extractCI(fit.result, "lambda", 1, confidence)
-        secondProportionCI = .extractCI(fit.result, "lambda", 2, confidence)
-        
-        cidf = data.frame(firstcompCI, secondcompCI, firstProportionCI, secondProportionCI)
-        #append to list
-        ciList = c(ciList,list(t(cidf)))
+        #add column/row names to the dataframe and then add the dataframe to list
+        colnames(out.df) = c("Estimate", "CI.lower", "CI.upper", "Std.Error")
+        rownames(out.df) = c(meannames,proportionnames)
+        ciList[i] = list(out.df)
     }
+    
     
     names(ciList) = inputNames
     #output .csv file has similar format as that of fitNormDistr
