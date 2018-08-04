55f4b0a49f3427d8d808ad2c7e64124d31f6926a
ysung6 <ysung6@jhu.edu>
2018-08-04 10:47:18 -0400

    fitNormDistr - got rid of meanRange parameter, standard error calculation, added strict constraints for multicomponent cases.

--- R/fitNormDistr.R
+++ R/fitNormDistr.R
@@ -11,18 +11,18 @@
 ##'
 ##' @usage
 ##' fitNormDistr(dcoef,components=NULL,log.transform=F,binwidth=NULL,combine.plot=F,output=F,
-##'              seed=NULL,meanRange=NULL, proportion=NULL, means=NULL, sd=NULL)
+##'              seed=NULL, proportion=NULL, means=NULL, sd=NULL)
 ##' @param dcoef diffusion coefficient calculated from Dcoef().
 ##' @param components parameter specifying the number of components to fit. If NULL (default), a components analysis is done to determine the most likely components and this number is then used for subsequent analysis.
 ##' @param log.transform logical indicate if log10 transformation is needed, default F.
 ##' @param binwidth binwidth for the combined plot. If NULL (default), will automatic assign binwidth.
 ##' @param combine.plot Logical indicate if all the plot should be combined into one, with same scale (/same axises breaks), same color theme, and same bin size for comparison.
-##' @param output Logical indicate if output file should be generated.
+##' @param output logical indicate if output file should be generated.
 ##' @param seed Seed for random number generator. This makes each run easily repeatable. Seed will be automatically assigned if no seed is specified (default). The seed information is stored as an attribute of the returned object. The seed can also be output to a txt file when output=T.
-##' @param meanRange list of vectors, each of which have the min&max that estimates a range where a peak of the distribution should lie.
 ##' @param proportion numeric vector with estimates of each component's proportion of the whole data.
 ##' @param means numeric vector with estimates of mean(mu) values for each component.
 ##' @param sd numeric vector with estimates of standard deviation(sigma) values for each component.
+##' @param constrain logical indicate if mean and std deviation are set to the given value. This will not work for the unimodal distribution.
 ##' @details
 ##' components analysis uses the likelihood ratio test (LRT) to assess the number of mixture components.
 ##' Bad Random seed generation may cause normalmixEM to crash. Running the function again would be the quickest solution to this issue.
@@ -50,8 +50,14 @@
 ##' mapply(identical,a[[1]],b[[1]])
 ##' #try with log transformation
 ##' c=fitNormDistr(dcoef,components=2,log.transform=T,combine.plot=F,output=F)
-##' #trying with some parameters provided(this will be applied to all dcoef results)
-##' d=fitNormDistr(dcoef,meanRange=list(c(0.1,0.3), c(0.5,0.7)))
+##' #trying with some parameters provided(this will be applied to all dcoef results). with constrain = F, this will be used as the starting values for the EM-algorithm
+##' #normally we should deal with only one dataset when working with constrains, since it will apply to all of them.
+##' folder3=system.file("extdata","HSF", package="sojourner")
+##' trackll=compareFolder(c(folder3),input=2)
+##' MSD=msd(trackll=trackll)
+##' dcoef=Dcoef(MSD,dt=6,plot=T,output=F)
+##' #try with constrain =T, the values will be forced to eqaul the provided ones.
+##' e=fitNormDistr(dcoef,means=c(0.3,0.5), constrain=T)
 
 
 ##' @export fitNormDistr
@@ -72,16 +78,20 @@
 }
 
 #function that deals with one-component normal distribution fitting
-.singlecompFit=function(data){
-    fit.info = fitdist(data=data, distr="norm")
+.singlecompFit=function(data, mean=NULL, sd=NULL, constrain=F){
+    if (constrain == T){warning("single component fitting cannot be constrained.")}
+    #    if(!is.null(mean) & !is.null(sd)){stop("Please provide only one of the mean or sd constraints.")}
+    #    fixlist = list(c(mean=mean, sd=sd))
+    #    fit.info = fitdist(data,"norm", fix.arg=list(mean=mean))
+    #} else{fit.info = fitdist(data=data, distr="norm")}
     #This dummy matrix is used to make it work out with the gg.mixEM function in Plotting.Helpers.R
+    fit.info = fitdist(data=data, distr="norm")
     dummy.posterior = matrix()
     colnames(dummy.posterior) = c("comp1")
     #a list with components similar to that of mixEM so that it can be used in gg.mixEM
     fit.output = list(x=as.vector(data), lambda = c(1), mu = fit.info$estimate[1], sigma = fit.info$sd[1],
                       loglik = fit.info$loglik, restarts=0, ft="normalmixEM", posterior= dummy.posterior)
-    fit.se = list(mu.se= c(fit.info$estimate[2]), sigma.se = c(fit.info$sd[2]), lambda.se = c(0.00))
-    return(list(fit.output,fit.se))
+    return(list(fit.output))
 }
 
 #uses mclust package's mclustBootstrapLRT() function to estimate the number of components in distribution
@@ -94,21 +104,17 @@
     return(components.int)
 }
 
-.getSummaryResult=function(result, result.se, log.transform){
+.getSummaryResult=function(result, log.transform){
     out=list(
-        data.frame(proportion=result$lambda,
-                   proportion.se=result.se$lambda.se),
-        
+        data.frame(proportion=result$lambda),
+
         #why returning it back from log?
         #data.frame(mean=if(log.transform) 10^(result$mu)
         #           else result$mu),
         data.frame(mean=result$mu),
-        mean.se=as.vector(result.se$mu.se),
         
-        
-        data.frame(sd=result$sigma,
-                   sd.se=result.se$sigma.se),
-        
+        data.frame(sd=result$sigma),
+
         log.lik=result$loglik
     )
     
@@ -118,23 +124,20 @@
     return(out)
 }
 
-.fitNormDistr=function(dcoef,components=NULL,log.transform=F,binwidth=NULL,combine.plot=F,output=F,meanRange=NULL,proportion=NULL,means=NULL,sd=NULL, seed=NULL){
+.fitNormDistr=function(dcoef,components=NULL,log.transform=F,binwidth=NULL,combine.plot=F,output=F,proportion=NULL,means=NULL,sd=NULL,seed=NULL,constrain=F){
     # scale=1e3
     if(is.null(seed)){stop("NULL seed, cannot execute .fitNormDistr")}
     name=names(dcoef)
     len=length(dcoef)
     
     #parameters that help initial settings.
-    params = list(meanRange, proportion, means, sd)
+    params = list(proportion, means, sd)
 
     mixmdl.lst=list()
     length(mixmdl.lst)=len
     names(mixmdl.lst)=name
 
     # store the standard error, currently only for components more than 2
-    mixmdl.se.lst=list()
-    length(mixmdl.se.lst)=len
-    names(mixmdl.se.lst)=name
     
     ## This part deals with input related errors
     #collection of parameter vector lengths
@@ -168,27 +171,21 @@
             } else{components.int=components}
         }
         
-        if(!is.null(meanRange)){
-            if (length(param.len) == 1){
-                #try to estimate/extract mu and sigma values from the given ranges
-                means=unlist(lapply(meanRange, mean))
-                diffs=lapply(meanRange, function(x){max(x)-mean(x)})
-                sd=unlist(diffs)/1.6
-            }
-            else{stop("Either only meanRange or any of proportion/mean/sd should be provided")}
-        }
         
         if (components.int == 1){ #The single component is a special case here since it cannot be a mixEM class object
-            oneComp=.singlecompFit(data=data)
+            if(constrain==T){
+                oneComp = .singlecompFit(data=data, mean=means, sd=sd, constrain=T)
+            } else {oneComp=.singlecompFit(data=data)}
             fit.output=oneComp[[1]]
-            fit.se=oneComp[[2]]
             plot.mixEM=gg.mixEM(fit.output,binwidth=binwidth,reorder=F)
             mixmdl=fit.output
             mixmdl.lst[[i]] = fit.output
-            mixmdl.se=fit.se
-            mixmdl.se.lst[[i]] = fit.se
         } else{
-            mixmdl=normalmixEM(data,k=components.int,maxit=1e4,epsilon=1e-10, lambda=proportion, mu=means, sigma=sd)
+            if(constrain == T) {
+                mixmdl=normalmixEM(data,k=components.int,maxit=1e4,epsilon=1e-10,lambda=proportion,sd.constr=sd,mean.constr=means)
+            } else {
+                mixmdl=normalmixEM(data,k=components.int,maxit=1e4,epsilon=1e-10,lambda=proportion,mu=means,sigma=sd)
+            }
             # reorder also for plotting and for output result
             # this is recordered on mean value
             mixmdl=reorderEM(mixmdl)
@@ -196,10 +193,8 @@
             plot.mixEM=gg.mixEM(mixmdl,binwidth=binwidth,reorder=T)
             # approximate standard error using parametic bootstrap
             cat("\napproximating standard error by parametic bootstrap...\n\n")
-            capture.output({mixmdl.se=boot.se(mixmdl, B = 1)})
             # file="/dev/null" # this only for mac
             mixmdl.lst[[i]]=mixmdl
-            mixmdl.se.lst[[i]]=mixmdl.se
         }
         suppressMessages(plot(plot.mixEM))
     }
@@ -209,7 +204,7 @@
         names(result.lst)=name
 
         for (i in 1:len){
-            result=.getSummaryResult(mixmdl.lst[[i]], mixmdl.se.lst[[i]], log.transform)
+            result=.getSummaryResult(mixmdl.lst[[i]], log.transform)
             result.lst[[i]]=result
         }
 
@@ -277,12 +272,12 @@
 }
 
 
-fitNormDistr=function(dcoef,components=NULL,log.transform=F,binwidth=NULL,combine.plot=F,output=F,seed=NULL, meanRange=NULL, proportion=NULL, means=NULL, sd=NULL){
+fitNormDistr=function(dcoef,components=NULL,log.transform=F,binwidth=NULL,combine.plot=F,output=F,seed=NULL, proportion=NULL, means=NULL, sd=NULL, constrain=F){
     seed=.setSeed(seed=seed)
 
     # return
     structure(.fitNormDistr(dcoef=dcoef,components=components,log.transform=log.transform,binwidth=binwidth,combine.plot=combine.plot,output=output,
-                            meanRange=meanRange,proportion=proportion,means=means,sd=sd,seed=seed),seed=seed)
+                            proportion=proportion,means=means,sd=sd,constrain=constrain,seed=seed),seed=seed)
 }
 
 
@@ -292,5 +287,4 @@ fitNormDistr=function(dcoef,components=NULL,log.transform=F,binwidth=NULL,combin
 
 
 # TODO: 
-# isolate the boot.se part from fitNormDistr(), so the bootstrap is all in one
-# place.
+# constrain for the single component case should be implemented.
