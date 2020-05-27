0156e756029853a1aa11248bc6fd71d32a275957
Sheng Liu <sheng.liu@live.ca>
2020-05-27 16:43:16 -0700

    (re)merged exportTrackll; tested and updated namespace, help doc.

--- R/exportTrackll.R
+++ R/exportTrackll.R
@@ -7,14 +7,10 @@
 ##' @title exportTrackll
 ##' @rdname exportTrackll-methods
 ##' @docType methods
-##'
-
 ##' @description take in a list of track lists (trackll) and export it into 
 ##' row-wise (ImageJ Particle Tracker style) .csv files in the working directory
-
 ##' @usage 
 ##' exportTrackll(trackll, cores = 1)
-##' 
 ##' @param trackll A list of track lists.
 ##' @param cores Number of cores used for parallel computation. This can be 
 ##' the cores on a workstation, or on a cluster. Tip: each core will be 
@@ -32,10 +28,9 @@
 ##' recommended), it will just output the start frame of each track instead 
 ##' and will take noticeably longer.
 ##'
-##' It is not recommended that exportTrackll be run on merged list of track 
-##' lists (trackll).
-##' Also, ensure that the input trackll is a list of track lists and not just 
-##' a track list.
+##' It is not recommended that exportTrackll be run on merged list of track
+##' lists (trackll).Also, ensure that the input trackll is a list of track lists
+##' and not just a track list.
 ##' 
 ##' The naming scheme for each export is as follows:
 ##' 
@@ -44,52 +39,58 @@
 ##' @examples
 ##' folder=system.file('extdata','SWR1',package='sojourner')
 ##' trackll=createTrackll(folder=folder, input=3)
-##' #Basic function call to exportTrackll with 2 cores into current directory
+##' 
+##' # Basic function call to exportTrackll into current directory
 ##' exportTrackll(trackll)
 ##' 
-##' #Get current working directory
+##' # Import export save back into a trackll
+##' # Get current working directory
 ##' getwd()
-##' 
-##' #Import export save back into a trackll
 ##' trackll.2 <- createTrackll(folder = getwd(), input = 3)
+##' 
+##  @importFrom rowr cbind.fill
+##' @export .exportRowWise
 ##' @export exportTrackll
 
 ############################################################################### 
 
 #### .exportRowWise ####
 
-.exportRowWise = function(track.list) {
+## # Export one track list
+## .exportRowWise(trackl)
+
+.exportRowWise = function(trackl) {
     
     # Confirmation text of function call
     cat("\nWriting .csv row-wise output in current directory for", 
-        getTrackFileName(track.list), 
+        getTrackFileName(trackl), 
         "...\n")
     
     # Collect track file name
-    track.file.name <- getTrackFileName(track.list)
+    track.file.name <- getTrackFileName(trackl)
     
     # Check for frame record column
-    if (ncol(track.list[[1]]) != 3) {
+    if (ncol(trackl[[1]]) != 3) {
         
         # Rename track list as trajectory numbers
-        names(track.list) <- c(seq_along(track.list))
+        names(trackl) <- c(seq_along(trackl))
         
         # Combine track data frames by trajectory and reorder column
-        df <- bind_rows(track.list, .id = "Trajectory")[, c("Trajectory", 
+        df <- bind_rows(trackl, .id = "Trajectory")[, c("Trajectory", 
             "Frame", "x", "y", "z")]
         
     } else {
         # Empty data frame df to be written into the .csv
         df <- NULL
         
-        # Loop through every trajectory in input track.list
-        for (i in seq_along(track.list)) {
+        # Loop through every trajectory in input trackl
+        for (i in seq_along(trackl)) {
             
             # Create a data frame temp with trajectory, frame, and track 
             # coordinate data
             temp <- data.frame(Trajectory = i, 
-                Frame = getStartFrame(track.list, i), 
-                track.list[[i]][seq_len(3)])
+                Frame = getStartFrame(trackl, i), 
+                trackl[[i]][seq_len(3)])
             
             # Append data frame df with data frame temp
             df <- rbind(df, temp)
@@ -109,22 +110,22 @@
 
 # # Install packages and dependencies library(rowr)
 
-# .exportColWise = function(track.list) {
+# .exportColWise = function(trackl) {
     
 #     # Confirmation text of function call
 #     cat("\nWriting .csv column-wise output in current directory for", 
-#         getTrackFileName(track.list), "...\n")
+#         getTrackFileName(trackl), "...\n")
     
 #     frame.list <- list()
     
-#     # Loop through every trajectory in input track.list
-#     for (i in seq_along(track.list)) {
+#     # Loop through every trajectory in input trackl
+#     for (i in seq_along(trackl)) {
         
-#         start.frame = getStartFrame(track.list[i])
+#         start.frame = getStartFrame(trackl[i])
         
 #         frame.list <- c(frame.list, start.frame, 0, 0)
         
-#         temp <- track.list[[i]][seq_len(3)]
+#         temp <- trackl[[i]][seq_len(3)]
         
 #         if (i != 1) {
 #             df <- cbind.fill(df, temp, fill = 0)
@@ -143,7 +144,7 @@
 #     # of the widest text file on your machine.  Rowwise export preferred'
     
 #     # Write the data frame df into the .csv and display confirmation text
-#     file.name = paste("COL", getTrackFileName(track.list), ".csv", sep = "")
+#     file.name = paste("COL", getTrackFileName(trackl), ".csv", sep = "")
 #     # write(header, file = file.name, append = TRUE)
 #     write.table(df, file = file.name, row.names = FALSE, sep = ",")
 #     # ,append = TRUE
@@ -160,7 +161,7 @@ exportTrackll = function(trackll, cores = 1) {
     
     if (cores == 1) {
         export = lapply(trackll, function(x) {
-            .exportRowWise(track.list = x)
+            .exportRowWise(trackl = x)
         })
     } else {
         # parallel excecute above block of code
@@ -170,17 +171,18 @@ exportTrackll = function(trackll, cores = 1) {
         
         cat("Initiated parallel execution on", cores, "cores\n")
         
-        # use outfile='' to display result on screen
+        # use outfile="" to display result on screen
         cl <- parallel::makeCluster(spec = cores, type = "PSOCK", outfile = "")
         # register cluster
         parallel::setDefaultCluster(cl)
         
         # pass environment variables to workers
-        parallel::clusterExport(cl, varlist = c(".exportRowWise"), 
-            envir = environment())
+        parallel::clusterExport(cl,
+                                varlist=c(".exportRowWise"),
+                                envir=environment())
         
-        export = parallel::parLapply(cl, trackll, function(x) {
-            .exportRowWise(track.list = x)
+        export = parallel::parLapply(cl,trackll,function(x){
+            .exportRowWise(trackl = x)
         })
         
         # stop cluster
@@ -189,3 +191,15 @@ exportTrackll = function(trackll, cores = 1) {
     }
     
 }
+
+
+## TODO:
+# identical(trackll,trackll.2)
+# FALSE
+
+# the track index is lost when export, therefore when import it has to be
+# regnerated. export also the track index, so one can import it and create
+# exactly the same
+
+
+
