6982675cb95e5ed20d7fb5079b13285773099c0c
Sheng Liu <sheng.liu@live.ca>
2020-05-27 17:09:40 -0700

    (re)merged helperFunctions()

--- R/helperFunctions.R
+++ R/helperFunctions.R
@@ -2,15 +2,21 @@
 #
 ################################################################################
 
+
+
+
+
+
 ##-----------------------------------------------------------------------------
-## .timeStamp add time stamp and file name as a unique signature of the
+## .timeStamp
+## add time stamp and file name as a unique signature of the
 ## output file @export .timeStamp
-.timeStamp = function(filename) {
+.timeStamp=function(filename) {
     
-    basename = basename(filename)
-    name = unlist(strsplit(basename, split = "[.]"))
-    fileName = paste(name[1], "-", format(Sys.time(), "%Y%m%d_%H%M%S"), 
-        sep = "")
+    basename=basename(filename)
+    name=unlist(strsplit(basename, split="[.]"))
+    fileName=paste(name[1], "-", format(Sys.time(), "%Y%m%d_%H%M%S"), 
+        sep="")
     return(fileName)
     
 }
@@ -19,10 +25,10 @@
 ## .valid
 
 # validity check for dt less than track length (-1)
-.valid = function(dt, track) {
+.valid=function(dt, track) {
     
     # get track length of all tracks
-    tracklen = dim(track)[1]
+    tracklen=dim(track)[1]
     
     if (dt > (tracklen - 1)) {
         stop("\ntrack length:\t", dim(track)[1], "\ndt:\t\t", dt, 
@@ -31,10 +37,10 @@
 }
 
 
-.valid = function(dt, track) {
+.valid=function(dt, track) {
     
     # get track length of all tracks
-    tracklen = dim(track)[1]
+    tracklen=dim(track)[1]
     
     if (dt > (tracklen - 1)) {
         stop("\ntrack length:\t", dim(track)[1], "\ndt:\t\t", dt, 
@@ -51,25 +57,25 @@
 ## tracks.msda2sjr
 
 ## @export tracks.msda2sjr
-tracks.msda2sjr = function(file) {
-    
-    tracks.file = readMat(file)
+tracks.msda2sjr=function(file){
+
+    tracks.file=readMat(file)
     # file.name=basename(file)
-    tracks.mat = tracks.file$tracks
-    
-    trackl.sjr = lapply(tracks.mat, function(x) {
-        x = data.frame(x)
-        x = x[, -1]  # remove time column
-        x = x/0.107  # change micrometer to pixel
-        colnames(x) = c("x", "y")
-        x$z = rep(1, times = dim(x)[1])
+    tracks.mat=tracks.file$tracks
+
+    trackl.sjr=lapply(tracks.mat, function(x){
+        x=data.frame(x)
+        x=x[,-1] # remove time column
+        x=x/0.107  # change micrometer to pixel
+        colnames(x)=c("x","y")
+        x$z=rep(1,times=dim(x)[1])
         return(x)
     })
-    
-    trackll.sjr = list(trackl.sjr)
-    names(trackll.sjr) = basename(file)
+
+    trackll.sjr=list(trackl.sjr)
+    names(trackll.sjr)=basename(file)
     return(trackll.sjr)
-    
+
 }
 
 # convert msda to sojourner format not used in functions yet
@@ -77,23 +83,23 @@ tracks.msda2sjr = function(file) {
 ##-----------------------------------------------------------------------------
 ## same.scale
 
-same.scale = function(mixmdl.lst) {
+same.scale=function(mixmdl.lst) {
     
-    scale = list()
-    length(scale) = length(mixmdl.lst)
-    names(scale) = names(mixmdl.lst)
+    scale=list()
+    length(scale)=length(mixmdl.lst)
+    names(scale)=names(mixmdl.lst)
     
     for (i in seq_along(mixmdl.lst)) {
         
-        den = density(mixmdl.lst[[i]]$x)
-        scale.x = c(min = min(den$x), max = max(den$x))
-        scale.y = c(min = min(den$y), max = max(den$y))
-        scale[[i]] = data.frame(scale.x, scale.y)
+        den=density(mixmdl.lst[[i]]$x)
+        scale.x=c(min=min(den$x), max=max(den$x))
+        scale.y=c(min=min(den$y), max=max(den$y))
+        scale[[i]]=data.frame(scale.x, scale.y)
     }
     
-    scale.df = do.call(rbind.data.frame, scale)
-    scale.same = data.frame(apply(scale.df, 2, function(x) {
-        c(min = min(x), max = max(x))
+    scale.df=do.call(rbind.data.frame, scale)
+    scale.same=data.frame(apply(scale.df, 2, function(x) {
+        c(min=min(x), max=max(x))
     }))
     
     return(scale.same)
@@ -101,55 +107,76 @@ same.scale = function(mixmdl.lst) {
 }
 
 ##-----------------------------------------------------------------------------
+## seedIt
+## @export seedIt
+seedIt=function(expr,seed){
+    if (is.null(seed)){
+        seed=sample(0:647,1)
+        set.seed(seed)
+    }else{set.seed(seed)}
+
+    note <- paste("\nRandom number generation seed",seed,"\n")
+    cat(note)
+    structure(expr, seed=seed)
+}
+
+
+##------------------------------------------------------------------------------
+## getStartFrame
 ## getStartFrame returns starting frame of a track/trajectory (using its
 ## name) at a given index for a track list
 
-# PARAMETERS: track.list = named track list output Note: Last five
-# characters of the original file name without extension (cannot
-# contain '.') index = index of the track in the track list (track
-# number)
+# PARAMETERS: track.list,named track list output 
+# Note: Last five characters of the original file name without extension (cannot
+# contain '.') 
+# index, index of the track in the track list (track number)
 
 ## @export getStartFrame
-getStartFrame = function(track.list, index) {
+getStartFrame=function(track.list, index) {
     return(as.numeric(substr(names(track.list[index]), 
-        gregexpr(pattern = "\\.", 
-        names(track.list[index]))[[1]][1] + 1, gregexpr(pattern = "\\.", 
+        gregexpr(pattern="\\.", 
+        names(track.list[index]))[[1]][1] + 1, gregexpr(pattern="\\.", 
         names(track.list[index]))[[1]][2] - 1)))
 }
 
 ##-----------------------------------------------------------------------------
+## getTrackFileName
 ## getTrackFileName returns the shortened file name of the track
 
-# PARAMETERS: track.list = named track list output Note: Last five
-# characters of the original file name without extension (cannot
+# PARAMETERS: track.list, named track list output 
+# Note: Last five characters of the original file name without extension (cannot
 # contain '.')
 
 ## @export getTrackFileName
-getTrackFileName = function(track.list) {
-    return(substr(names(track.list[1]), 1, gregexpr(pattern = "\\.", 
+getTrackFileName=function(track.list) {
+    return(substr(names(track.list[1]), 1, gregexpr(pattern="\\.", 
         names(track.list[1]))[[1]][1] - 1))
 }
 
 ##-----------------------------------------------------------------------------
+## abTrack
 ## abTrack returns absolute corrdinates of a track (for plotting)
 
-# PARAMETERS: track = track input to be transformed into absolute
+# PARAMETERS:
+# PARAMETERS: track=track input to be transformed into absolute
 # coordinates
 
 ## @export abTrack
-abTrack = function(track) {
-    min.x = min(track$x)
-    min.y = min(track$y)
-    return(data.frame(x = track$x - min.x, y = track$y - min.y))
+abTrack=function(track) {
+    min.x=min(track$x)
+    min.y=min(track$y)
+    return(data.frame(x=track$x - min.x, y=track$y - min.y))
 }
 ##-----------------------------------------------------------------------------
+## removeFrameRecord
 ## removeFrameRecord remove frame record for backwards compatibility
 
-# PARAMETERS: track.list = track list with frame record in the fourth
+#PARAMETERS:
+# PARAMETERS: track.list=track list with frame record in the fourth
 # column
 
 ## @export removeFrameRecord
-removeFrameRecord = function(track.list) {
+removeFrameRecord=function(track.list) {
     for (i in seq_along(track.list)) {
         track.list[[i]] <- track.list[[i]][-c(4)]
     }
@@ -157,12 +184,21 @@ removeFrameRecord = function(track.list) {
 }
 
 convert.abtrackll=function(trackll){
-    ab.trackll = list()
+    ab.trackll=list()
     for(i in 1:length(trackll)){
-        segl = trackll[[i]]
-        segl = lapply(trackll[[i]], abTrack)
-        ab.trackll[[i]] = segl
+        segl=trackll[[i]]
+        segl=lapply(trackll[[i]], abTrack)
+        ab.trackll[[i]]=segl
     }
-    names(ab.trackll) = names(trackll)
+    names(ab.trackll)=names(trackll)
     return (ab.trackll)
 }
+
+##-----------------------------------------------------------------------------
+#TODO: 
+
+# add a one line description to all the helper functions, and maybe output a map
+# of function for the helpers as well. it apears all the helpers has been
+# exported, so maybe there is no need to sync this function between sojourner
+# and sojourner.pro.
+
